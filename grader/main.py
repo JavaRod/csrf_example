72f6a69613f37c3ab69ffb60267f0ba814bb0247
Rod Musser <rmusser@yahoo.com>
2020-11-28 11:51:27 -0800

    Fixed CSRF attack

--- grader/main.py
+++ grader/main.py
@@ -2,7 +2,8 @@ import os
 import base64
 
 from flask import Flask, request, session
-from model import Grade 
+from model import Grade
+import random
 
 app = Flask(__name__)
 app.secret_key = "dFbVyccvTuNpkUP2x6vcnZWN"
@@ -10,14 +11,18 @@ app.secret_key = "dFbVyccvTuNpkUP2x6vcnZWN"
 @app.route('/', methods=['GET', 'POST'])
 def home():
 
+    if 'csrf_token' not in session:
+        session['csrf_token'] = str(random.randint(10000000, 99999999))
+
     if request.method == 'POST':
-        g = Grade(
-            student=request.form['student'],
-            assignment=request.form['assignment'],
-            grade=request.form['grade'],
-        )
-        #print("(" + request.form['grade'] + ")")
-        g.save()
+        if request.form.get('csrf_token', None) == session['csrf_token']:
+            g = Grade(
+                student=request.form['student'],
+                assignment=request.form['assignment'],
+                grade=request.form['grade'],
+            )
+            #print("(" + request.form['grade'] + ")")
+            g.save()
 
     body = """
 <html>
@@ -35,12 +40,14 @@ def home():
     <label for="grade">Grade</label>
     <input type="text" name="grade"><br>
 
+    <input type="hidden" name="csrf_token" value="{}">
+
     <input type="submit" value="Submit">
 </form>
 
 <h2>Existing Grades</h2>
-"""
-    
+""".format(session['csrf_token'])
+
     for g in Grade.select():
         body += """
 <div class="grade">
@@ -48,7 +55,7 @@ def home():
 </div>
 """.format(g.student, g.assignment, g.grade)
 
-    return body 
+    return body
 
 
 if __name__ == "__main__":
